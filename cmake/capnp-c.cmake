60118052c14eca3419e7d4e2c61ddc95ea3dedb8
Walter Schell <walterschell@users.noreply.github.com>
2020-05-07 08:44:34 -0400

    Bugfix: missed a library that needed to be xcompiled instead of native

--- cmake/capnp-c.cmake
+++ cmake/capnp-c.cmake
@@ -10,7 +10,7 @@ function(require_targets)
     endif()
 endfunction()
 
-require_targets(capnp capnp-c capnp_c) 
+require_targets(native-capnp native-capnp-c capnp_c) 
 
 function(add_capnproto schema_file)
     get_filename_component(basename ${schema_file} NAME)
@@ -19,12 +19,12 @@ function(add_capnproto schema_file)
     message(STATUS "Will build ${basename}.c")
     add_custom_command(
         OUTPUT ${basename}.c ${basename}.h
-        DEPENDS ${actual_source} capnp capnp-c
-        COMMAND capnp compile -o $<TARGET_FILE:capnp-c> -I $<TARGET_PROPERTY:capnp-c,capnp_include_path> --src-prefix=${actual_source_dir} ${actual_source}
+        DEPENDS ${actual_source} native-capnp native-capnp-c
+        COMMAND native-capnp compile -o $<TARGET_FILE:native-capnp-c> -I $<TARGET_PROPERTY:native-capnp-c,capnp_include_path> --src-prefix=${actual_source_dir} ${actual_source}
         #COMMAND echo "hello" > ${basename}.c
     )
     get_filename_component(libname ${schema_file} NAME_WE)
     add_library(${libname} STATIC ${CMAKE_CURRENT_BINARY_DIR}/${basename}.c)
-    target_include_directories(${libname} PUBLIC ${CMAKE_CURRENT_BINARY_DIR} $<TARGET_PROPERTY:capnp-c,capnp_include_path>)
+    target_include_directories(${libname} PUBLIC ${CMAKE_CURRENT_BINARY_DIR} $<TARGET_PROPERTY:native-capnp-c,capnp_include_path>)
     target_link_libraries(${libname} PUBLIC capnp_c)
 endfunction()
